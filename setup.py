636f2f41a70f80cec0ae47addcd02dfc02319d12
Kristen Thyng <kthyng@gmail.com>
2024-01-24 13:42:44 -0600

    made `ptm` entry point

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
         "write_to": "particle_tracking_manager/_version.py",
         "write_to_template": '__version__ = "{version}"',
         "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
-    }
+    },
+    entry_points={"console_scripts": ["ptm=particle_tracking_manager.cli:main"]},
 )
