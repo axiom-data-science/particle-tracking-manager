766b6645b10209fd368f78d167d446993b449f31
Kristen Thyng <kthyng@gmail.com>
2025-01-31 15:19:26 -0800

    updates to get tests working locally
    One still does not work but it will with a change in my next PR

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -15,7 +15,7 @@ def test_setup():
     No drifters are run due to oceanmodel=None
     """
     ret_value = os.system(
-        f"python {ptm.__path__[0]}/cli.py ocean_model=test lon=-151 lat=59 use_auto_landmask=True start_time='2000-1-1'"
+        f"ptm ocean_model=test lon=-151 lat=59 use_auto_landmask=True start_time='2000-1-1' --dry-run"
     )
     assert ret_value == 0
 
