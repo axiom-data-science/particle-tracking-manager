7cbdea1b43267dea24c6e8018bcd0793c0a850bd
Kristen Thyng <kthyng@gmail.com>
2025-04-09 08:54:38 -0700

    updated seeding test

--- tests/test_seeding.py
+++ tests/test_seeding.py
@@ -8,6 +8,7 @@ import pytest
 import particle_tracking_manager as ptm
 
 
+@pytest.mark.slow
 def test_seeding_from_geojson():
     """Seed from GeoJSON."""
 
@@ -18,11 +19,11 @@ def test_seeding_from_geojson():
             "type": "Polygon",
             "coordinates": [
                 [
-                    [-151.0, 58.0],
-                    [-150.0, 58.0],
-                    [-150.0, 59.0],
-                    [-151.0, 59.0],
-                    [-151.0, 58.0],
+                    [-90, 28.7],
+                    [-90, 28.8],
+                    [-90.1, 28.8],
+                    [-90.1, 28.7],
+                    [-90, 28.7],
                 ]
             ],
         },
@@ -35,12 +36,15 @@ def test_seeding_from_geojson():
         steps=1,
         lon=None,
         lat=None,
+        ocean_model="TXLA",
+        ocean_model_local=False,
+        start_time=datetime(2009, 11, 19, 12, 0),
     )
     m.add_reader()
     m.seed()
 
-    expected_lon = [-150.51787, -150.51787]
-    expected_lat = [58.25654, 58.25654]
+    expected_lon = [-90.06226, -90.06226]
+    expected_lat = [28.733112, 28.733112]
 
     assert np.allclose(m.initial_drifters.lon, expected_lon)
     assert np.allclose(m.initial_drifters.lat, expected_lat)
