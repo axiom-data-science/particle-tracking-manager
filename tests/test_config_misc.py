ba067d00985870a5ffc203caa6f19556c9efb19c
Honza Rychly <honza@axiomdatascience.com>
2025-12-09 15:15:23 -0900

    Fix file tests for windows
    - silly microsoft and their reverse slashes :)

--- tests/test_config_misc.py
+++ tests/test_config_misc.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pytest
 
 from particle_tracking_manager.config_misc import SetupOutputFiles
@@ -6,7 +8,7 @@ from particle_tracking_manager.config_misc import SetupOutputFiles
 @pytest.mark.parametrize("suffix", ["", ".nc", ".parq", ".parquet", ".txt"])
 def test_log_name(suffix):
     m = SetupOutputFiles(output_file=f"outputs/newtest{suffix}")
-    assert m.logfile_name == "outputs/newtest.log"
+    assert m.logfile_name == str(Path("outputs/newtest.log"))
 
 
 @pytest.mark.parametrize("format, suffix", [("netcdf", ".nc"), ("parquet", ".parquet")])
